5f6e7e76d3b3f0527342111edab829a5a05f9430
A01254123 <A01254123@itesm.mx>
2021-09-03 16:31:31 +0000

    PromedioSencillo_CesarMtz

--- assignments/05PromedioSencillo/src/exercise.py
+++ assignments/05PromedioSencillo/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    n=int(input())
+    suma=0
+    for i in range(n):
+        b=float(input())
+        suma=suma+b
+    prom=suma/n
+    print(prom)
 if __name__=='__main__':
     main()
